33940098244bbc78f5bb9892bf842e1cd88c2fd9
HP_3500 <sergeymaksimov78@yandex.ru>
2026-02-18 10:13:48 +0700

    add migrate

--- app/models/post.py
+++ app/models/post.py
@@ -1,5 +1,9 @@
+from datetime import datetime
 from ..extensions import db
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    teacher = db.Column(db.String(250))
     subject = db.Column(db.String(250))
+    student = db.Column(db.String(250))
+    date = db.Column(db.DateTime, default=datetime)
