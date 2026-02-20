237c9b5341a189c7dd583c3e1ff2aeb3a1156fc9
HP_3500 <sergeymaksimov78@yandex.ru>
2026-02-20 11:57:14 +0700

    add icon awesome

--- app/routes/post.py
+++ app/routes/post.py
@@ -9,6 +9,7 @@ def all():
     posts = Post.query.all()
     return render_template('post/all.html', posts=posts)
 
+
 @post.route('/post/create', methods=['POST', 'GET'])
 def create():
     if request.method == 'POST':
@@ -27,6 +28,27 @@ def create():
             return redirect('/')
         except Exception as e:
             print(e)
-
     else:
         return render_template('post/create.html')
+
+
+@post.route('/post/<int:id>/update', methods=['POST', 'GET'])
+def update(id):
+    if request.method == 'POST':
+        teacher = request.form.get('teacher')
+        subject = request.form.get('subject')
+        student = request.form.get('student')
+        print(teacher)
+        print(subject)
+        print(student)
+
+        post = Post(teacher=teacher, subject=subject, student=student)
+
+        try:
+            db.session.add(post)
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            print(e)
+    else:
+        return render_template('post/update.html')
