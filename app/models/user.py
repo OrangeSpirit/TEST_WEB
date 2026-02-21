71dabe5d4d8ab15187b22c5a1929d4adf42bed3e
HP_3500 <sergeymaksimov78@yandex.ru>
2026-02-21 14:50:40 +0700

    edit user.py add column

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,12 @@
+from datetime import datetime, timezone
 from ..extensions import db
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String(50), default='user')
     name = db.Column(db.String(50))
+    login = db.Column(db.String(50))
+    password = db.Column(db.String(200))
+    date = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    avatar = db.Column(db.String(200))
